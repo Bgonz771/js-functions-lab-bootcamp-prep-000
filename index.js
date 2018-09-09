22b4b875ea5b23c79fba9e0633285600430e1036
billy@arkhamgc.com <billy@arkhamgc.com>
2018-09-09 15:17:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ function holidayCountdown(holiday, days){
   
 }
 
+
+today=new Date();
+var cmas=new Date(today.getFullYear(), 11, 25);
+if (today.getMonth()==11 && today.getDate()>25)
+{
+cmas.setFullYear(cmas.getFullYear()+1);
+}
+var one_day=1000*60*60*24;
+console.log(Math.ceil((cmas.getTime()-today.getTime())/(one_day))+
+" days left until Christmas!");
+
+
+
+
+
 today=new Date();
 var cmas=new Date(today.getFullYear(), 11, 25);
 if (today.getMonth()==11 && today.getDate()>25) 
