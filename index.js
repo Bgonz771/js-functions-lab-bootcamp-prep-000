b6e5f6c79493e18f5a2ca0ee7175bc6f317fa3c9
billy@arkhamgc.com <billy@arkhamgc.com>
2018-09-09 16:12:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ console.log(Math.ceil((cmas.getTime()-today.getTime())/(one_day))+
 
 
 
-today=new Date();
-var cmas=new Date(today.getFullYear(), 11, 25);
+//today=new Date();
+//var cmas=new Date(today.getFullYear(), 11, 25);
 if (today.getMonth()==11 && today.getDate()>25) 
 {
 cmas.setFullYear(cmas.getFullYear()+1); 
