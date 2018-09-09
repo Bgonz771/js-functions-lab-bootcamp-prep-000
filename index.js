5e363db2a894f59c5e3d56df25c2160a2d2a93a4
billy@arkhamgc.com <billy@arkhamgc.com>
2018-09-09 16:40:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,22 +12,14 @@ function happyHolidayTo(holiday, name){
   
 }
 
-function holidayCountdown(holiday, days){
+
   
   
-}
 
 
-today=new Date();
-var thanksGiving=new Date(today.getFullYear(), 11, 22);
-if (today.getMonth()==11 && today.getDate()>22)
-{
-thanksGiving.setFullYear(thanksGiving.getFullYear()+1);
-}
-var one_day=1000*60*60*24;
-console.log(Math.ceil(thanksGiving.getTime()-today.getTime())/(one_day))+
-"days left until ThanksGiving!");
-}
+
+
+
 
 
 
